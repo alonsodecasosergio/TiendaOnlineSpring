5b076a8dcff5b42120febf279dcebe3e203567f9
Sergio <alonsodecaso.sergio@gmail.com>
2021-05-05 16:54:06 +0200

    Creacion del formulario de registro si eres un cliente

--- src/main/java/com/tienda/controller/LoginController.java
+++ src/main/java/com/tienda/controller/LoginController.java
@@ -1,10 +1,14 @@
 package com.tienda.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tienda.model.Entities.Usuario;
 
 @Controller
 @RequestMapping("login")
@@ -15,6 +19,26 @@ public class LoginController {
 		
 		return "login/login";
 	}
+	
+	@GetMapping("registrar")
+	public String registrar(Model model) {
+		
+		model.addAttribute("usuario", new Usuario());
+		
+		return "login/registrar";
+	}
+	
+	@PostMapping("registrar/validar")
+	public String registrarValidar(Model model, @ModelAttribute Usuario usuario) {
+		
+		usuario.setRol(3);
+		System.out.println(usuario.toString());
+		
+		return "login/login";
+		
+	}
+		
+	
 	
 	@PostMapping("validaracceso")
 	public String validar(@RequestParam String login, @RequestParam String password) {
