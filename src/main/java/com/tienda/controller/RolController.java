61af9d8774b3a31014a4f046002d99d4f5a636f2
Sergio <alonsodecaso.sergio@gmail.com>
2021-05-07 08:12:50 +0200

    Creacion de la vista y servicio de roles generando una lista con los roles disponibles

--- src/main/java/com/tienda/controller/RolController.java
+++ src/main/java/com/tienda/controller/RolController.java
@@ -1,28 +1,38 @@
 package com.tienda.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tienda.service.RolService;
+
 @Controller
-@RequestMapping("rol")
+@RequestMapping("/rol")
 public class RolController {
 	
-	@PostMapping("alta")
+	@Autowired
+	private RolService rs;
+	
+	@PostMapping("/alta")
 	public String alta() {
 		
 		return "";
 	}
 	
-	@PostMapping("baja")
+	@PostMapping("/baja")
 	public String baja() {
 		
 		return "";
 	}
 	
-	@PostMapping("listar")
-	public String listar() {
+	@GetMapping("/listar")
+	public String listar(Model model) {
 		
-		return "";
+		model.addAttribute("roles", rs.getAll());
+		
+		return "roles/list";
 	}
 }
--- src/main/java/com/tienda/service/RolService.java
+++ src/main/java/com/tienda/service/RolService.java
@@ -1,5 +1,19 @@
 package com.tienda.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tienda.model.Entities.Rol;
+import com.tienda.repository.RolRepository;
+
+@Service
 public class RolService {
 
+	@Autowired
+	private RolRepository repository;
+	
+	public Iterable<Rol> getAll(){
+		
+		return repository.findAll();
+	}
 }
