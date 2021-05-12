13e790251eee57a0d7db7627e4b24cbfdbe9e48e
Sergio <alonsodecaso.sergio@gmail.com>
2021-05-12 13:00:47 +0200

    Añadida la funcionalidad de editar los valores de configuracion

--- src/main/java/com/tienda/controller/ConfiguracionController.java
+++ src/main/java/com/tienda/controller/ConfiguracionController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tienda.model.Entities.Configuracion;
 import com.tienda.service.ConfiguracionService;
 
 @Controller
@@ -30,6 +33,14 @@ public class ConfiguracionController {
 		model.addAttribute("configuracion", cs.getConfigById(id));
 		
 		return "configuracion/new";
+	}
+	
+	@PostMapping("/edit/submit")
+	public String validar(Model model, @ModelAttribute Configuracion config) {
+		
+		cs.addConfig(config);
+		
+		return "redirect:/configuracion/listar";
 	}
 
 }
